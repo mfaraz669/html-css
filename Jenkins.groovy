668fcbbf5511efd8e1928faff7f814948c789c17
Mohammed Faraz <43960417+mfaraz669@users.noreply.github.com>
2024-03-31 11:20:37 +0530

    Add files via upload

---
+++ Jenkins.groovy
@@ -0,0 +1,58 @@
+pipeline {
+    agent any {
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    gitCheckout (
+                        branch: "main",
+                        url: "https://github.com/zeenathfatima-123/html-css.git"
+                    )
+                        
+                }
+            }
+
+            stage('Install Dependencies') {
+                steps {
+                    nodejs('21.5.0') {
+                        sh 'npm install'
+                    }
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    // Build the application
+                    nodejs('21.5.0')
+                    sh 'npm run build'
+                }
+            }
+
+            stage('Static code analysis') {
+                environment {
+                    SONAR_URL = ""
+                }
+                steps {
+                    withCredentials([string(credentialsId: 'sonarqube', variable: 'SONAR_AUTH_TOKEN')]) {
+                        //RUN SONARQUBE TO ANALYSE CODE
+                    withSonarQubeEnv('SonarQube-Server') {
+                        sh 'sonar-scanner' -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.host.url=${SONAR_URL}}
+                    }
+                }
+            }
+            
+            stage("Quality Gate") {
+                steps {
+                    timeout(time: 1, unit: 'HOURS') {
+                        waitForQualityGate(abortPipeline: true)
+                    }
+                }
+            }
+            
+        }
+            
+    }
+
+
+        
+}
